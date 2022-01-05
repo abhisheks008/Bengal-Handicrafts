1e3f7fb4290987cae895d3954fc20ad344b0306e
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2022-01-05 11:35:26 +0530

    Add files via upload

---
+++ resources/backend/sidebar.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-expand-md navbar-light">
+<button class="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="#myNavbar">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="myNavbar">
+    <div class="container-fluid">
+        <div class="row">
+        <!-- sidebar -->
+        <div class="col-xl-2 col-lg-3 col-md-4 sidebar fixed-top ">
+            <span class="navbar-brand d-block mx-auto text-center py-3 mb-4 bottom-border">Havenly Beds Online Store Admin</span>
+            <div class="bottom-border pb-3">
+                <img src="images/logo.png" width="50" class="rounded-circle mr-3">
+                <span>Chiko Mutandwa</span>
+            </div>
+            <ul class="navbar-nav flex-column mt-4">
+                <li class="nav-item">
+                    <a href="index.php?dashboard" class="nav-link p-3 mb-2 current"><i class="fas fa-home fa-lg mr-3"></i>Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?products" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-tasks fa-lg mr-3"></i>View Products
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?categories" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-tasks fa-lg mr-3"></i>View Categories
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?orders" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-shopping-cart fa-lg mr-3"></i>View Sales
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?addproducts" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-chart-line fa-lg mr-3"></i>Add Products
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?addcategories" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-chart-bar fa-lg mr-3"></i>Add Categories
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?adduser" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-user-plus fa-lg mr-3"></i>Add Users
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?slider" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas fa-file-video-o fa-lg mr-3"></i>Slider
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?users" class="nav-link p-3 mb-2 sidebar-link">
+                        <i class="fas  fa-users fa-lg mr-3"></i>View Users
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- /sidebar -->
---
+++ resources/backend/topnav.php
@@ -0,0 +1,44 @@
+
+
+            <!-- top-nav -->
+            <div class="col-xl-10 col-lg-9 col-md-8 ml-auto  fixed-top py-2 top-navbar">
+                <div class="row align-items-center">
+
+                    <div class="col-md-4">
+                    <!-- <h4 class="text-uppercase mb-0">Dashboard</h4> -->
+                    </div>
+                    <div class="col-md-5">
+                    </div>
+                    <div class="col-md-3">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ml-md-auto"><a href="#" class="nav-link" data-toggle="modal" data-target="#sign-out"><i class="fas fa-sign-out-alt text-danger fa-lg"></i></a></li>
+                    </ul>
+                    </div>
+                    
+                </div>
+            </div>
+            <!-- /top-nav -->
+            </div>
+        </div>
+    </div>
+</nav>
+
+        <!-- modal -->
+        <div class="modal fade" id="sign-out">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Want to leave?</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        Press logout to leave
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-success" data-dismiss="modal">Stay Here</button>
+                       <a href="logout.php" class="btn btn-danger"> Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /modal -->
